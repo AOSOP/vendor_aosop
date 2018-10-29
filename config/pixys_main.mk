0a4d8f87dea361ffd9159946095393158a95a94a
amarbajpai <bajpai.amar3@gmail.com>
2018-10-29 11:18:06 +0530

    Add Google Sans family
    Change-Id: I66be86c64bf02340a81c9e370a8a93e7e0753707

--- config/pixys_main.mk
+++ config/pixys_main.mk
@@ -6,7 +6,8 @@ PRODUCT_PACKAGES += \
      PixelLauncher3 \
      PixysSettings \
      Turbo \
-     CameraRoll
+     CameraRoll \
+     OmniRecord
 
 # Markup libs
 ifeq ($(TARGET_ARCH),arm64)
@@ -30,6 +31,9 @@ include vendor/pixys/themes/config.mk
 
 # Pixys props
 $(call inherit-product, vendor/pixys/config/pixys_props.mk)
+
+# Fonts
+include vendor/pixys/config/fonts.mk
 
 # Call Overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/pixys/overlay/common
